223a66b832af8851aa5e0033fe6831bf32ef8044
Dieter Prado <dayterp@gmail.com>
2021-07-14 09:20:01 -0500

    adding files

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBean2Implement.java
@@ -0,0 +1,8 @@
+package com.fundamentos.springboot.fundamentos.bean;
+
+public class MyBean2Implement implements MyBean{
+    @Override
+    public void print() {
+        System.out.println("Hola desde una implementacion propia del bean 2 juas juas");
+    }
+}
---
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -0,0 +1,23 @@
+package com.fundamentos.springboot.fundamentos.bean;
+
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
+public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
+    Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImplement.class);
+
+    private MyOperation myOperation;
+
+    public MyBeanWithDependencyImplement(MyOperation myOperation) {
+        this.myOperation = myOperation;
+    }
+
+    @Override
+    public void printWithDependency() {
+        LOGGER.info("Hemos ingresado al metodo printwithdependency");
+        int numero = 1;
+        LOGGER.debug("EL numero enviado como parametro a la dependencia operacion es: " +  numero);
+        System.out.println(myOperation.sum(numero));
+        System.out.println("Hola desde la implementacion de un bean con independencia");
+    }
+}
